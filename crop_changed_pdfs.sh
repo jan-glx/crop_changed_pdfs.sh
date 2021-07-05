564a9879758cf3c0d11d988744976e79e6d6f5ec
Jan Gleixner <jan.gleixner@gmail.com>
2021-07-05 17:56:02 +0200

    add missing quotation and remove surplus $

--- crop_changed_pdfs.sh
+++ crop_changed_pdfs.sh
@@ -1,11 +1,12 @@
+#!/bin/env bash
 path=$(echo "/$1" | sed -e 's/\\/\//g' -e 's/\([A-Z]\):/mnt\/\l\1/')
 echo "cropping files in $path/*.pdf"
 #echo "cropping files in::   $1"
-for file in $path/*.pdf
+for file in "$path"/*.pdf
 do
-if [[ $file != *"-crop.pdf"* ]]; then
-	file2=$(echo $file | sed 's/\.pdf$/-crop\.pdf$/g')
-	#echo "$file -- $file2 $([ ! -f $file2 ]) $([ "$file" -ot "$file2" ])"
+if [[ "$file" != *"-crop.pdf"* ]]; then
+	file2=$(echo "$file" | sed 's/\.pdf$/-crop\.pdf/g')
+	# echo "$file -- $file2 $([ ! -f "$file2" ]) $([ "$file" -ot "$file2" ])"
 	if [ ! -f "$file2" ] || [ "$file" -nt "$file2" ]; then
 		pdfcrop "$file"
 		echo "cropped $file"
